daaf953047d3050f316b23899380bc369bc85a6c
AronAhn <39235119+AronAhn@users.noreply.github.com>
2018-10-14 22:34:20 +0900

    Update 4344_Aron.py

--- Baekjun/4344_Aron.py
+++ Baekjun/4344_Aron.py
@@ -9,4 +9,5 @@ for case in cases:
 	n_student, *scores = (map(int, case.split(" ")))
 	mean_score = sum(scores)/n_student
 	above_average = sum(map(lambda x: x>mean_score, scores))
+	#above_average = sum([score>mean_score for score in scores])
 	print("{0:.3f}%".format(round(above_average*100/n_student, 3)))
